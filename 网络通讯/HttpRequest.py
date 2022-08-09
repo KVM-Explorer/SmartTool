74b5899943b5480c111eec7a1cf481b3e53f481f
Geek <wfcyywh1225@126.com>
2022-08-09 11:00:10 +0800

    新增部分网络通讯Post请求

--- 网络通讯/HttpRequest.py
+++ 网络通讯/HttpRequest.py
@@ -1,3 +1,4 @@
+import json
 import aiohttp
 import asyncio
 
@@ -12,7 +13,23 @@ class HttpRequest:
                 self.status = respond.status
                 self.content = await respond.text(encoding='utf-8')
 
+    async def postRequest(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=self.url) as respond:
+                self.status = respond.status
+                self.content = await respond.text(encoding='utf-8')
 
+    '''
+        发起Post请求，附带输入信息header和payload，如果payload非字符串会被进行转码处理,使用前请先对数据进行加密等处理
+    '''
+    async def postRequestWithPayload(self,header,payload:json):
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url=self.url,headers=header,data = payload) as respond:
+                self.status = respond.status
+                self.content = await respond.text(encoding='utf-8')
+    
+
+    
 if __name__ == "__main__":
     client = HttpRequest('http://python.org')
     loop = asyncio.get_event_loop()
